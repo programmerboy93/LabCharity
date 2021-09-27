383230d4a1864ec758732636ff94035f8f0b3fd8
Łukasz Sikorski <lukaszsikorski19933@gmail.com>
2021-09-28 00:00:35 +0200

    added entity category and repository

---
+++ src/main/java/pl/coderslab/charity/entity/Category.java
@@ -0,0 +1,24 @@
+package pl.coderslab.charity.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Entity
+@Getter
+@Setter
+@Table(name="categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotEmpty
+    @Column(length = 30)
+    @Size(min=3,max=30)
+    private String name;
+}
