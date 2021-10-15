45f8f73722fba6c35afe5df8513be3b39cabaf81
Łukasz Sikorski <lukaszsikorski19933@gmail.com>
2021-10-15 22:34:37 +0200

    add address register/**

--- src/main/java/pl/coderslab/charity/security/WebSecurityConfig.java
+++ src/main/java/pl/coderslab/charity/security/WebSecurityConfig.java
@@ -42,7 +42,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/login", "/register", "/").permitAll()
+                .antMatchers("/login", "/register/**", "/").permitAll()
                 .antMatchers("/**").authenticated()
                 .and()
                 .formLogin()
